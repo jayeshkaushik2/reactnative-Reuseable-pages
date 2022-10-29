b8987f84899e3538de1f2fa9da3f26028aec4a2c
jayeshkaushik2 <jayeshkaushik2@gmail.com>
2022-10-29 21:34:58 +0530

    added env and env.sample files

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import Login from "./app/screens/Login";
 import Signup from "./app/screens/Signup";
+import { ECOM_API_URL } from "@env";
 
 export default function App() {
+  console.log(ECOM_API_URL);
   return (
     <>
-      <Signup />
-      {/* <Login /> */}
+      {/* <Signup /> */}
+      <Login />
     </>
   );
 }
